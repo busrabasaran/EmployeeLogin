152fbae5282c1fdee4687f4275ffd631344cbdf6
büsra başaran <busrab92@hotmail.com>
2019-06-24 14:52:42 +0300

    .

---
+++ EmployeeLogin/EmployeeLogin/Controllers/EmployeeController.cs
@@ -0,0 +1,22 @@
+﻿using EmployeeLogin.DAL;
+using EmployeeLogin.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EmployeeLogin.Controllers
+{
+    public class EmployeeController : Controller
+    {
+
+        [MyAuthenticationFilter]
+        // GET: Employee
+        public ActionResult UserDetails()
+        {
+            var employee = Session["Employee"] as Employee;
+            return View(employee);
+        }
+    }
+}
